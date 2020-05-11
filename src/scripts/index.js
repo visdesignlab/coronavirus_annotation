ef59eeaf9fd5e9f9cfdc3d978c2bdd2e7012a699
Jen Rogers <jen@sci.utah.edu>
2020-05-11 13:46:59 -0600

    added canvas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,5 +5,8 @@ import * as vid from './video_player';
 console.log('webpack starterkit');
 
 let mainWrap = document.getElementById('main-wrap');
+let vidWrap = document.getElementById('vid-wrap');
+let canvasWrap = document.getElementById('canvas-wrap');
 
-vid.addVidPlayer(mainWrap);
+vid.formatVidPlayer(vidWrap);
+vid.formatCanvas(canvasWrap);
--- src/scripts/video_player.js
+++ src/scripts/video_player.js
@@ -1,15 +1,25 @@
 import * as d3 from 'd3';
 
-export function addVidPlayer(div){
-  
-    let player = d3.select(div).append('video');
-    player.node().controls = true;
-    let src = player.append('source');
+export function formatVidPlayer(div){
+
+    let video = d3.select(div).select('video');
+    video.node().controls = true;
+    let src = video.append('source');
+
     src.attr('src', './public/covid.mp4');
     src.attr('type', "video/mp4");
 
-    return player;
-    
+    return div;
+     
+}
+
+export function formatCanvas(div){
+
+    let canvas = d3.select(div).select('canvas');
+    canvas.attr('id', 'vid-canvas');
+
+    return div;
+
 }
 
 
