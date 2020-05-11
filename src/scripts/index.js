0d983b46aff1449e73dfc9a55a93b366cc3a0990
Jen Rogers <jen@sci.utah.edu>
2020-05-11 15:22:11 -0600

    can draw on video

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,8 +5,8 @@ import * as vid from './video_player';
 console.log('webpack starterkit');
 
 let mainWrap = document.getElementById('main-wrap');
-let vidWrap = document.getElementById('vid-wrap');
-let canvasWrap = document.getElementById('canvas-wrap');
+// let vidWrap = document.getElementById('vid-wrap');
+// let canvasWrap = document.getElementById('canvas-wrap');
 
-vid.formatVidPlayer(vidWrap);
-vid.formatCanvas(canvasWrap);
+vid.formatVidPlayer(mainWrap);
+vid.formatCanvas(mainWrap);
--- src/scripts/video_player.js
+++ src/scripts/video_player.js
@@ -3,6 +3,8 @@ import * as d3 from 'd3';
 export function formatVidPlayer(div){
 
     let video = d3.select(div).select('video');
+    video.attr('id', 'video');
+
     video.node().controls = true;
     let src = video.append('source');
 
@@ -15,12 +17,61 @@ export function formatVidPlayer(div){
 
 export function formatCanvas(div){
 
-    let canvas = d3.select(div).select('canvas');
-    canvas.attr('id', 'vid-canvas');
+    let canvas = d3.select(div).select('canvas').node();
+    canvas.setAttribute('id', 'vid-canvas');
+
+    const context = canvas.getContext("2d");
+    let videoDim = document.getElementById('video').getBoundingClientRect();
+    console.log(video, 'size')
+
+    canvas.width = videoDim.width;
+    canvas.height = videoDim.height;
+
+    context.strokeStyle = "red";
+
+    var oldX, oldY;
+    var draw=false;
+
+    function oMousePos(element, evt) {
+        var ClientRect = element.getBoundingClientRect();
+             return { //objeto
+             x: Math.round(evt.clientX - ClientRect.left),
+             y: Math.round(evt.clientY - ClientRect.top)
+        }
+    }  
+
+    div.onmousedown=function(e) {
+
+        oldX = (e.pageX - this.offsetLeft);
+        oldY = (e.pageY - this.offsetTop);
+   
+        draw=true;
+    }
+    div.onmousemove=function(e) {
+      
+      var mouseX = (e.pageX - this.offsetLeft);
+      var mouseY = (e.pageY - this.offsetTop);
+    
+      if(draw) {
+        console.log(e)
+      context.beginPath();
+      context.moveTo(oldX, oldY);
+      context.lineTo(mouseX, mouseY);
+      context.stroke();
+      context.closePath();
+      oldX=mouseX;
+      oldY=mouseY;
+      }
+    
+    }
+    div.onmouseup=function(e) {
+      draw=false;
+    }
 
     return div;
 
 }
+
 
 
 
