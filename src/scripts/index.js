17b71c2cc1b458c87db851f4691829fc2e88c38d
Jen Rogers <jen@sci.utah.edu>
2020-05-12 13:00:45 -0600

    added firebase

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,34 @@
 import '../styles/index.scss';
 import * as vid from './video_player';
 import { renderNav } from './sidebar';
+import * as firebase from 'firebase';
+
+
+/**
+ * Firebase shenannigans
+ */
+
+ // Your web app's Firebase configuration
+ var firebaseConfig = {
+    apiKey: "AIzaSyC7cqpyFRK5Pzh6tReSEhU0zdfhXAaRbeU",
+    authDomain: "covid-annotation.firebaseapp.com",
+    databaseURL: "https://covid-annotation.firebaseio.com",
+    projectId: "covid-annotation",
+    storageBucket: "covid-annotation.appspot.com",
+    messagingSenderId: "297369575962",
+    appId: "1:297369575962:web:be320c5d86a0b719a467a3",
+    measurementId: "G-H9JT7JZCZ8"
+  };
+  // Initialize Firebase
+  let app = firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  var database = firebase.database();
+
+  console.log('database', database, app);
+
+/**
+ * End Firebase shennanigans
+ */
 
 const nav = [
     {"key":"Draw", "callback":vid.formatCanvas}
