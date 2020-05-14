5dcd462a7394a14cf6d0c75435e4619ae5c53c58
Jen Rogers <jen@sci.utah.edu>
2020-05-14 15:22:10 -0600

    messed up canvas but got a nice play pack control

--- src/scripts/video_player.js
+++ src/scripts/video_player.js
@@ -4,17 +4,112 @@ const shapeArray = [];
 
 export function formatVidPlayer(div, videoPath){
 
-    let video = d3.select(div).select('video');
-    video.attr('id', 'video');
+  let video = d3.select(div).select('video');
+  video.attr('id', 'video');
 
-    video.node().controls = true;
-    let src = video.append('source');
+    
+  let src = video.append('source');
 
-    src.attr('src', videoPath);
-    src.attr('type', "video/mp4");
+  src.attr('src', videoPath);
+  src.attr('type', "video/mp4");
 
-    return div;
+  customControls(video.node());
+    
+  return div;
      
+}
+
+function customControls(videoNode){
+
+  const videoControls = document.getElementById('video-controls');
+
+  const videoWorks = !!document.createElement('video').canPlayType;
+  if (videoWorks) {
+    videoNode.controls = false;
+    videoControls.classList.remove('hidden');
+  }else{
+    videoNode.controls = true;
+  }
+
+  const playButton = document.getElementById('play');
+  // Add eventlisteners here
+  playButton.addEventListener('click', togglePlay);
+
+  const playbackIcons = document.querySelectorAll('.playback-icons use');
+
+  videoNode.addEventListener('play', updatePlayButton);
+  videoNode.addEventListener('pause', updatePlayButton);
+
+  const timeElapsed = document.getElementById('time-elapsed');
+  const duration = document.getElementById('duration');
+
+  videoNode.addEventListener('loadedmetadata', initializeVideo);
+
+  videoNode.addEventListener('timeupdate', updateTimeElapsed);
+
+  const progressBar = document.getElementById('progress-bar');
+  const seek = document.getElementById('seek');
+
+  function initializeVideo() {
+    const videoDuration = Math.round(videoNode.duration);
+    seek.setAttribute('max', videoDuration);
+    progressBar.setAttribute('max', videoDuration);
+    const time = formatTime(videoDuration);
+    duration.innerText = `${time.minutes}:${time.seconds}`;
+    duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+  }
+
+  // updateTimeElapsed indicates how far through the video
+  // the current playback is
+  function updateTimeElapsed() {
+    const time = formatTime(Math.round(videoNode.currentTime));
+    timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+    timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+  }
+
+  // initializeVideo sets the video duration, and maximum value of the
+  // progressBar
+
+  function initializeVideo() {
+    const videoDuration = Math.round(videoNode.duration);
+    const time = formatTime(videoDuration);
+    duration.innerText = `${time.minutes}:${time.seconds}`;
+    duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+  }
+
+  // formatTime takes a time length in seconds and returns the time in
+  // minutes and seconds
+  function formatTime(timeInSeconds) {
+    const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+
+    return {
+      minutes: result.substr(3, 2),
+      seconds: result.substr(6, 2),
+    };
+  };
+
+  // updatePlayButton updates the playback icon and tooltip
+  // depending on the playback state
+  function updatePlayButton() {
+    playbackIcons.forEach(icon => icon.classList.toggle('hidden'));
+  
+    if (videoNode.paused) {
+      playButton.setAttribute('data-title', 'Play (k)')
+    } else {
+      playButton.setAttribute('data-title', 'Pause (k)')
+    }
+  }
+  // togglePlay toggles the playback state of the video.
+  // If the video playback is paused or ended, the video is played
+  // otherwise, the video is paused
+  function togglePlay() {
+    if (videoNode.paused || videoNode.ended) {
+      videoNode.play();
+    } else {
+      videoNode.pause();
+    }
+  }
+
 }
 
 export function formatCanvas(){
