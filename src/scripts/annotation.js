d236b82c1538246084d7f2438a253288e7d8d44e
Jen Rogers <jen@sci.utah.edu>
2020-07-02 19:29:43 -0600

    button bool set up

--- src/scripts/annotation.js
+++ src/scripts/annotation.js
@@ -8,6 +8,7 @@ import { firebaseConfig, checkDatabase, dataKeeper } from './firebaseStuff';
 import { annotationType, defaultTemplate, annotationTemplate } from './templates';
 import { dropDown, clearSidebar } from './canvas';
 import { image } from 'd3';
+import { specialUserCheck, addCommentButton } from './topToolbar';
 
 export const currentUserKeeper = [];
 export const specialUserKeeper = [];
@@ -39,6 +40,8 @@ if(mainWrap){
         
           checkDatabase(ref, updateSideAnnotations);
           checkDatabase(ref, specialUserCheck);
+
+          d3.select('.add-comment').select('button').on('click', (d, i, n)=> addCommentButton(d, i, n));
           clearSidebar();
 
         // User is signed in.
@@ -51,60 +54,5 @@ if(mainWrap){
     });
 }
 
-  function specialUserCheck(dbRef){
-
-    let specialUserList = d3.entries(dbRef['special-users']).map(m=> m.key);
-
-    let currentUser = firebase.auth().currentUser;
-
-    let vidDim = d3.select('video').node().getBoundingClientRect();
-
-    if(specialUserList.indexOf(currentUser.uid) > -1){
-
-      let userWrap = d3.select('.user-wrap');
-      userWrap.selectAll('*').remove();
-     
-      userWrap.append('text').text(`Signed in as Admin: ${currentUser.displayName}`);
-      let bugLink = userWrap.append('a');
-      bugLink.attr('href', 'https://github.com/visdesignlab/coronavirus_annotation/issues');
-      bugLink.attr('target', "_blank");
-      bugLink.append('span').classed("fas fa-bug", true);
-
-      let newAnno = [...annotationType];
-      newAnno.push({key:'annotation', tag:'annotation', tempCall: annotationTemplate});
-
-      let tagButton = dropDown(d3.select('#annotation-wrap-r'), newAnno, 'Type of Comment', 'ann-type-drop', currentUser, null, true);
-      d3.select('#annotation-wrap-r').append('div').classed('template-wrap', true);
-
-      var storage = firebase.storage();
-      var storageRef = storage.ref();
-                    
-      let interDIV = d3.select('#interaction');
-      interDIV.attr('width', vidDim.width).attr('height', vidDim.height);
-
-    }else{
-
-      let userWrap = d3.select('.user-wrap');
-      userWrap.selectAll('*').remove();
-      userWrap.append('text').text(`Signed in as: ${currentUser.displayName}`);
-
-      let bugLink = userWrap.append('a');
-      bugLink.attr('href', 'https://github.com/visdesignlab/coronavirus_annotation/issues');
-      bugLink.attr('target', "_blank");
-      bugLink.append('span').classed("fas fa-bug", true);
-
-      let tagButton = dropDown(d3.select('#annotation-wrap-r'), annotationType, 'Type of Comment', 'ann-type-drop', currentUser, null, true);
-      d3.select('#annotation-wrap-r').append('div').classed('template-wrap', true);
-
-      var storage = firebase.storage();
-      var storageRef = storage.ref();
-                    
-      let interDIV = d3.select('#interaction');
-      interDIV.attr('width', vidDim.width).attr('height', vidDim.height);
-
-    }
-
-
-
-  }
+  
 
---
+++ src/scripts/topToolbar.js
@@ -0,0 +1,80 @@
+import '../styles/index.scss';
+import * as d3 from 'd3';
+import * as vid from './video_player';
+import { renderNav, toggleMagic, updateSideAnnotations} from './sidebar';
+import * as firebase from 'firebase';
+import "firebase/auth";
+import { firebaseConfig, checkDatabase, dataKeeper } from './firebaseStuff';
+import { annotationType, defaultTemplate, annotationTemplate } from './templates';
+import { dropDown, clearSidebar } from './canvas';
+import { image } from 'd3';
+
+export function addCommentButton(d, i, n){
+    console.log(d, i, n)
+    if(n[i].value === 'off'){
+        n[i].value = 'on';
+        d3.select('#sidebar').select('#annotation-wrap').selectAll('*').remove();
+    }else{
+        n[i].value = 'off';
+        let ref = firebase.database().ref();  
+        checkDatabase(ref, updateSideAnnotations);
+    }
+}
+    
+
+export function specialUserCheck(dbRef){
+
+    let specialUserList = d3.entries(dbRef['special-users']).map(m=> m.key);
+
+    let currentUser = firebase.auth().currentUser;
+
+    let vidDim = d3.select('video').node().getBoundingClientRect();
+
+    if(specialUserList.indexOf(currentUser.uid) > -1){
+
+      let userWrap = d3.select('.user-wrap');
+      userWrap.selectAll('*').remove();
+     
+      userWrap.append('text').text(`Signed in as Admin: ${currentUser.displayName}`);
+      let bugLink = userWrap.append('a');
+      bugLink.attr('href', 'https://github.com/visdesignlab/coronavirus_annotation/issues');
+      bugLink.attr('target', "_blank");
+      bugLink.append('span').classed("fas fa-bug", true);
+
+      let newAnno = [...annotationType];
+      newAnno.push({key:'annotation', tag:'annotation', tempCall: annotationTemplate});
+
+      let tagButton = dropDown(d3.select('#annotation-wrap-r'), newAnno, 'Type of Comment', 'ann-type-drop', currentUser, null, true);
+      d3.select('#annotation-wrap-r').append('div').classed('template-wrap', true);
+
+      var storage = firebase.storage();
+      var storageRef = storage.ref();
+                    
+      let interDIV = d3.select('#interaction');
+      interDIV.attr('width', vidDim.width).attr('height', vidDim.height);
+
+    }else{
+
+      let userWrap = d3.select('.user-wrap');
+      userWrap.selectAll('*').remove();
+      userWrap.append('text').text(`Signed in as: ${currentUser.displayName}`);
+
+      let bugLink = userWrap.append('a');
+      bugLink.attr('href', 'https://github.com/visdesignlab/coronavirus_annotation/issues');
+      bugLink.attr('target', "_blank");
+      bugLink.append('span').classed("fas fa-bug", true);
+
+      let tagButton = dropDown(d3.select('#annotation-wrap-r'), annotationType, 'Type of Comment', 'ann-type-drop', currentUser, null, true);
+      d3.select('#annotation-wrap-r').append('div').classed('template-wrap', true);
+
+      var storage = firebase.storage();
+      var storageRef = storage.ref();
+                    
+      let interDIV = d3.select('#interaction');
+      interDIV.attr('width', vidDim.width).attr('height', vidDim.height);
+
+    }
+
+
+
+  }
